054762d1a3f6e35f2db2fb91a65ce5c794ac2aa7
ignacio alonso <ignacioanacional@gmail.com>
2022-03-03 15:38:14 -0300

    Arreglos en el codigo

--- Desafio4-Clase.js
+++ Desafio4-Clase.js
@@ -4,12 +4,13 @@ class Contenedor {
     constructor(_archivo) {
         this.archivo = _archivo
         this.productos = []
+        this.fs = require('fs')
     }
-
+    
     async save(_producto) {
-        const fs = require('fs')
+        
         try {
-            const data = await fs.promises.readFile(this.archivo, 'utf-8')
+            const data = await this.fs.promises.readFile(this.archivo, 'utf-8')
             this.productos = JSON.parse(data)
             _producto.id = this.productos[this.productos.length - 1].id + 1;
             this.productos.push(_producto)
@@ -23,7 +24,7 @@ class Contenedor {
         }
 
         try {
-            await fs.promises.writeFile(this.archivo, JSON.stringify(this.productos, null, 2))
+            await this.fs.promises.writeFile(this.archivo, JSON.stringify(this.productos, null, 2))
             return _producto.id;
         } catch (e) {
             console.log('El archivo o la ruta no existen.')
@@ -31,9 +32,8 @@ class Contenedor {
     }
 
     async getById(_id) {
-        const fs = require('fs')
         try {
-            const data = await fs.promises.readFile(this.archivo, 'utf-8')
+            const data = await this.fs.promises.readFile(this.archivo, 'utf-8')
             this.productos = JSON.parse(data)
             let productoFiltrado = this.productos.filter((prod) => prod.id === _id)
             
@@ -45,9 +45,9 @@ class Contenedor {
     }
 
     async getAll() {
-        const fs = require('fs')
+        
         try {
-            const data = await fs.promises.readFile(this.archivo, 'utf-8')
+            const data = await this.fs.promises.readFile(this.archivo, 'utf-8')
             this.productos = JSON.parse(data)
 
             return JSON.stringify(this.productos, null, 2);
@@ -58,14 +58,14 @@ class Contenedor {
     }
 
     async deleteById(_id) {
-        const fs = require('fs')
+        
 
         try {
-            const data = await fs.promises.readFile(this.archivo, 'utf-8')
+            const data = await this.fs.promises.readFile(this.archivo, 'utf-8')
             this.productos = JSON.parse(data)
             let productosFiltrados = this.productos.filter((prod) => prod.id !== _id)
             try {
-                await fs.promises.writeFile(this.archivo, JSON.stringify(productosFiltrados, null, 2))
+                await this.fs.promises.writeFile(this.archivo, JSON.stringify(productosFiltrados, null, 2))
             } catch (e) {
                 console.log('El archivo o la ruta no existen.')
             }
@@ -76,9 +76,9 @@ class Contenedor {
     }
 
     async deleteAll() {
-        const fs = require('fs')
+        
         try {
-            await fs.promises.writeFile(this.archivo, '[]')
+            await this.fs.promises.writeFile(this.archivo, '[]')
         } catch (e) {
             console.log('El archivo o la ruta no existen.')
         }
--- servidor.js
+++ servidor.js
@@ -13,16 +13,41 @@ var Contenedor = require('./Desafio4-Clase.js');
 const Stock = new Contenedor('./productos.txt')
 
 app.get('/productos', (req, res)=> {
-    Stock.getAll().then(v => res.send((JSON.parse(v))))
+    Stock.getAll().then(v => {
+        const p = JSON.parse(v)
+        var resData = [];
+        p.map((producto) => {
+            
+            var data = 
+                `<h1> Producto ${producto.id} </h1> 
+             <div> Nombre: ${producto.title} </div>
+             <div> Precio: ${producto.price} </div>
+             <div> Imagen: ${producto.thumbnail} </div>`
+
+             resData = resData + data;
+        })
+
+        res.send(resData)
+        }
+    )
 })
 
 app.get('/', (req, res)=> {
-    res.send('<h1> Inicio </h1>')
+    res.send(`<h1> Inicio </h1> 
+             <div> /productos -> Todos los productos </div>
+             <div> /productos/id -> Busca el producto </div>
+             <div> /productoRandom -> Trae un producto random </div>`)
 })
 
 app.get('/productos/:productoId', (req, res)=> {
     Stock.getById(parseInt(req.params.productoId))
-    .then(v => res.send((JSON.parse(v))))
+    .then(v => {
+        const p = JSON.parse(v)
+        res.send(`<h1> Producto ${p.id} </h1> 
+             <div> Nombre: ${p.title} </div>
+             <div> Precio: ${p.price} </div>
+             <div> Imagen: ${p.thumbnail} </div>`)}
+             )
     .catch(err => res.send('No se encontró el producto'))
 })
 
@@ -31,7 +56,12 @@ app.get('/productoRandom', (req, res)=> {
         const productos = (JSON.parse(v))
         const prodcuto = (Math.floor(Math.random() * ((productos.length + 1) - 1)) + 1)
         Stock.getById(parseInt(prodcuto))
-        .then(v => res.send((JSON.parse(v))))
+        .then(v => {
+            const p = JSON.parse(v)
+            res.send(`<h1> Producto ${p.id} </h1> 
+                 <div> Nombre: ${p.title} </div>
+                 <div> Precio: ${p.price} </div>
+                 <div> Imagen: ${p.thumbnail} </div>`)})
         .catch(err => res.send('No se encontró el producto'))
     })
 })
